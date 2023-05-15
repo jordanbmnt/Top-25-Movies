e9dc948193c66656cc46a0ecfae4dd1ed7175bd9
Jordan Maurice Beaumont <jordanbmnt@gmail.com>
2023-05-15 18:12:02 +0200

    added cards

--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -1,10 +1,15 @@
-import { Paper } from '@mui/material'
+import "../Style/style.css";
+import { Paper, Typography } from '@mui/material'
  
 export default function CardList() {
   return (
     <Paper
     elevation={3}
-    sx={{ width: "calc(90vw/6 - 12px)", height: "103%", backgroundColor: "#1a232c", border: "1px solid #2a3741" }}
-  />
+    sx={{ width: "calc(90vw/6 - 12px)", height: "103%", backgroundColor: "#1a232c", border: "1px solid #2a3741", display: 'flex', flexFlow: 'column', alignItems: 'center' }}>
+      <Typography className='card-list-header'>Movie</Typography>
+      <Paper className="movie-card" elevation={3}/>
+      <Paper className="movie-card" elevation={3}/>
+      <Paper className="movie-card" elevation={3}/>
+    </Paper>
   )
 }
